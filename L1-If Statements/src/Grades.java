7375581fb39398d1f21609839c854f0ffc134e2f
truetdav000 <truetdav000@warren.k12.in.us>
2021-11-17 12:22:22 -0500

    fixed Mr. Law's big ol mistake

--- L1-If Statements/src/Grades.java
+++ L1-If Statements/src/Grades.java
@@ -4,6 +4,7 @@ public class Grades {
 
     public static void main(String[] args){
         Scanner read = new Scanner(System.in);
+        String grade = "";
 
         System.out.print("Please enter your percentage grade:");
         double percentage = read.nextDouble();
@@ -12,27 +13,20 @@ public class Grades {
             System.out.println("Your grade is F");
         }
 
-        if(percentage >= 60) {
-            if (percentage < 70) {
-                System.out.println("Your grade is D");
-            }
+        if(percentage >= 60){
+            System.out.println("Your grade is D");
         }
 
-        if(percentage >= 70) {
-            if (percentage < 80) {
-                System.out.println("Your grade is C");
-            }
+        if(percentage >= 70){
+            System.out.println("Your grade is C");
         }
 
-        if(percentage >= 80) {
-            if (percentage < 90) {
-                System.out.println("Your grade is B");
-            }
+        if(percentage >= 80){
+            System.out.println("Your grade is B");
         }
 
         if(percentage >= 90){
             System.out.println("Your grade is A");
         }
-
     }
 }
