c412bf97e31450daad2d0104ad7ed46aa64a8c58
truetdav000 <truetdav000@warren.k12.in.us>
2021-11-19 11:43:33 -0500

    lesson 3

--- L3-Random Numbers/src/QuizGame.java
+++ L3-Random Numbers/src/QuizGame.java
@@ -1,2 +1,44 @@
+import java.util.Random;
+
 public class QuizGame {
+    static int userAnswer, correctAnswer;
+    static String displayProblem = "";
+    public static void main(String[] args) {
+        
+    }
+    public static void addProblem(){
+        int num1 = randomNum();
+        int num2 = randomNum();
+
+        correctAnswer = num1+num2;
+        displayProblem = num1 + " + " + num2;
+    }
+    public static void subtProblem(){
+        int num1 = randomNum();
+        int num2 = randomNum();
+
+        correctAnswer = num1-num2;
+        displayProblem = num1 + " - " + num2;
+    }
+    public static void multProblem(){
+        int num1 = randomNum();
+        int num2 = randomNum();
+
+        correctAnswer = num1*num2;
+        displayProblem = num1 + " * " + num2;
+    }
+    public static void divideProblem(){
+        int num1 = randomNum();
+        int num2 = randomNum();
+
+        correctAnswer = num1/num2;
+        displayProblem = num1 + " / " + num2;
+    }
+
+    public static int randomNum(){
+        Random random = new Random();
+        return random.nextInt(20) + 1;
+    }
+
 }
+
